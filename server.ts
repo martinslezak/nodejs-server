6d2139924b2096d8c3a343a53095fc889e7cddae
unknown <martinsl@PC0236N.cross.cz>
2017-06-16 17:06:21 +0200

    first commit

---
+++ server.ts
@@ -0,0 +1,17 @@
+/// <reference path="include/node.d.ts" />
+
+// Configuration
+let port = 3000;
+let wwwroot = "wwwroot";
+
+// Import modules
+let express = require("express");
+let app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(wwwroot + "/" + req.uri);
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
